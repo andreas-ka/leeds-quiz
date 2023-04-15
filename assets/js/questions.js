6c94ee82e71af9d787657821c97eecfdb7766615
andreas-ka <>
2023-04-15 18:00:05 +0000

    added questions.js with all 20 questions

---
+++ assets/js/questions.js
@@ -0,0 +1,180 @@
+questions = []{
+    id: 0,
+          questions: "What year was Leeds United founded?",
+          answers: [  { text: "1920", isCorrect: false },
+                      { text: "1917", isCorrect: false },
+                      { text: "1919", isCorrect: true },
+                      { text: "1918", isCorrect: false }
+          ]
+  }
+  {
+    id: 1,
+          questions: "Who is the all time leading goalscorer for Leeds United?",
+          answers: [  { text: "Peter Lorimer", isCorrect: true },
+                      { text: "John Charles", isCorrect: false },
+                      { text: "Tom Jennings", isCorrect: false },
+                      { text: "Allan Clarke", isCorrect: false }
+          ]
+  }
+  {
+    id: 2,
+          questions: "Who was the latest Swedish player that played for Leeds United?",
+          answers: [  { text: "Rasmus Kristensen", isCorrect: false },
+                      { text: "Pontus Jansson", isCorrect: true },
+                      { text: "Tomas Brolin", isCorrect: false },
+                      { text: "Brendan Aaronson", isCorrect: false }
+          ]
+  }
+  {
+    id: 3,
+          questions: "During the 19/20 season Leeds won the Championship but what was our total points?",
+          answers: [  { text: "93", isCorrect: true },
+                      { text: "92", isCorrect: false },
+                      { text: "91", isCorrect: false },
+                      { text: "90", isCorrect: false }
+          ]
+  }
+  {
+    id: 4,
+          questions: "In the first season back in the Premier League with Bielsa, what rank did we end up at?",
+          answers: [  { text: "12", isCorrect: false },
+                      { text: "10", isCorrect: false },
+                      { text: "9", isCorrect: true },
+                      { text: "11", isCorrect: false }
+          ]
+  }
+  {
+    id: 5,
+          questions: "Who was the co-owner with Andrea Radrizzani before he bought the remaining 50%?",
+          answers: [  { text: "Mariano Chellini", isCorrect: false },
+                      { text: "Matteo Claudio", isCorrect: false },
+                      { text: "Maurizio Costa", isCorrect: false },
+                      { text: "Massimo Cellino", isCorrect: true }
+          ]
+  }
+  {
+    id: 6,
+          questions: "What answer is a nickname for Leeds United?",
+          answers: [  { text: "White Cockatoos", isCorrect: false },
+                      { text: "The Doves", isCorrect: false },
+                      { text: "The Peacocks", isCorrect: true },
+                      { text: "The Marchies", isCorrect: false }
+          ]
+  }
+  {
+    id: 7,
+          questions: "What is the record attendance at Elland Road in 1967?",
+          answers: [  { text: "57,892", isCorrect: true },
+                      { text: "55,625", isCorrect: false },
+                      { text: "52,992", isCorrect: false },
+                      { text: "56,122", isCorrect: false }
+          ]
+  }
+  {
+    id: 8,
+          questions: "How many members does Leeds United Supporters Club of Scandinavia have?",
+          answers: [  { text: "4000+", isCorrect: false },
+                      { text: "2500+", isCorrect: false },
+                      { text: "5000+", isCorrect: true },
+                      { text: "1500+", isCorrect: false }
+          ]
+  }
+  {
+    id: 9,
+          questions: "Who is the most expensive player we have bought?",
+          answers: [  { text: "Raphina", isCorrect: false },
+                      { text: "Georgino Rutter", isCorrect: true },
+                      { text: "Brendan Aaronson", isCorrect: false },
+                      { text: "Rodrigo Moreno", isCorrect: false }
+          ]
+  }
+  {
+    id: 10,
+          questions: "What team did we lose to in 2000/2001 Champions League semi-final?",
+          answers: [  { text: "Lazio", isCorrect: false },
+                      { text: "Real Madrid", isCorrect: false },
+                      { text: "Valencia", isCorrect: true },
+                      { text: "Deportivo La Coruña", isCorrect: false }
+          ]
+  }
+  {
+    id: 11,
+          questions: "What is the official fee we got when we sold Raphina to Barcelona (in pounds)?",
+          answers: [  { text: "£60,000,000", isCorrect: false },
+                      { text: "£58,000,000", isCorrect: false },
+                      { text: "£50,000,000", isCorrect: false },
+                      { text: "£55,000,000", isCorrect: true }
+          ]
+  }
+  {
+    id: 12,
+          questions: "How many Leeds players have been inducted into the English football Hall of fame?",
+          answers: [  { text: "11", isCorrect: true },
+                      { text: "8", isCorrect: false },
+                      { text: "10", isCorrect: false },
+                      { text: "12", isCorrect: false }
+          ]
+  }
+  {
+    id: 13,
+          questions: "During the 1960s what team inspired Revie to change our colours?",
+          answers: [  { text: "Juventus", isCorrect: false },
+                      { text: "Lyon", isCorrect: false },
+                      { text: "Schalke 04", isCorrect: false },
+                      { text: "Real Madrid", isCorrect: true }
+          ]
+  }
+  {
+    id: 14,
+          questions: "What team did we beat in the FA Cup final 1972?",
+          answers: [  { text: "Tottenham", isCorrect: false },
+                      { text: "Liverpool", isCorrect: false },
+                      { text: "Arsenal", isCorrect: true },
+                      { text: "Derby", isCorrect: false }
+          ]
+  }
+  {
+    id: 15,
+          questions: "How many goals did Patrick Bamford score during the 20/21 season back in the Premier League?",
+          answers: [  { text: "18", isCorrect: false },
+                      { text: "15", isCorrect: false },
+                      { text: "16", isCorrect: false },
+                      { text: "17", isCorrect: true }
+          ]
+  }
+  {
+    id: 16,
+          questions: "What team did Marcelo Bielsa coach before Leeds United?",
+          answers: [  { text: "Lazio", isCorrect: false },
+                      { text: "Lille", isCorrect: true },
+                      { text: "Marseille", isCorrect: false },
+                      { text: "Athletic Bilbao", isCorrect: false }
+          ]
+  }
+  {
+    id: 17,
+          questions: "What position had former Leeds coach David O'Leary as a player?",
+          answers: [  { text: "Right back", isCorrect: false },
+                      { text: "Midfielder", isCorrect: false },
+                      { text: "Centre back", isCorrect: true },
+                      { text: "Left back", isCorrect: false }
+          ]
+  }
+  {
+    id: 18,
+          questions: "In 2022 The Old Peacock pub changed its name temporarily to what?",
+          answers: [  { text: "The Dallas", isCorrect: false },
+                      { text: "The Bamford", isCorrect: false },
+                      { text: "Marching On", isCorrect: false },
+                      { text: "The Bielsa", isCorrect: true }
+          ]
+  }
+  {
+    id: 19,
+          questions: "Which shirt manufacturer did we have before Adidas took over 2020?",
+          answers: [  { text: "Diadora", isCorrect: false },
+                      { text: "Admiral", isCorrect: false },
+                      { text: "Macron", isCorrect: false },
+                      { text: "Kappa", isCorrect: true }
+          ]
+  }
