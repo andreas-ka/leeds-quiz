4901f99f08fba3eab93c28c0e6a73a1a1712b8f1
andreas-ka <>
2023-04-16 18:09:40 +0000

    added correct incorrect classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,19 +24,38 @@ function displayQuestion(index) {
 */
 
 function showQuestions(){
+    resetState()
     const que_text = document.querySelector(".questions-class");
 
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>'+ quiz[0].numb + ". " + quiz[0].question +'</span>';
     let option_tag = 
-    '<div class="answer-button"><span>'+ quiz[0].options[0] +'</span></div>'
-    + '<div class="answer-button"><span>'+ quiz[0].options[1] +'</span></div>'
-    + '<div class="answer-button"><span>'+ quiz[0].options[2] +'</span></div>'
-    + optionBtn + quiz[0].options[3] +'</div>';
-    questionText.innerHTML = que_tag; //adding new span tag inside que_tag
-    optionBtn.innerHTML = option_tag; //adding new div tag inside option_tag
-    console.log(quiz[index].options[0])
+    '<button class="answer-btn"><span>'+ quiz[0].options[0] +'</span></button>'
+    + '<button class="answer-btn"><span>'+ quiz[0].options[1] +'</span></button>'
+    + '<button class="answer-btn"><span>'+ quiz[0].options[2] +'</span></button>'
+    + '<button class="answer-btn"><span>'+ quiz[0].options[3] +'</span></button>';
+    questionText.innerHTML = que_tag; //adding new span tag inside question-class
+    answerBtnArea.innerHTML = option_tag; // adding new span with option in answer buttons
+    button.addEventListener('click', selectOption());
+    
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
 }
+
+function selectOption() {
+    let userAnswer = answer.textContent;
+    let correctAnswer = quiz.answer;
+    if (userAnswer === correctAnswer)
+    
+}
+
+function resetState() {
+    while(answerBtnArea.firstChild) {
+        answerBtnArea.removeChild(answerBtnArea.firstChild)
+    }
+}
+
 
 // start button action
 startQuizBtn.onclick = ()=> {
