63f20fe4ef3f90ffde70ace45753bef710fb43f9
andreas-ka <>
2023-04-16 15:08:28 +0000

    troubleshooting js code for grabbing and displaying questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,222 +1,222 @@
 /* Creating an Array containing all the questions and answers in boolean format */
-
-const questions = [
-    {   numb: 1,
-        questions: "What year was Leeds United founded?",
+const quiz = [
+    {   
+        numb: 1,
+        question: "What year was Leeds United founded?",
         answer: "1919",
-        alternative: [
+        options: [
             "1920",
             "1917", 
             "1919", 
-            "1918", 
+            "1918" 
           ]
   },
   {
         numb: 2,
-        questions: "Who is the all time leading goalscorer for Leeds United?",
+        question: "Who is the all time leading goalscorer for Leeds United?",
         answer: "Peter Lorimer",
-        alternative: [
+        options: [
             "Peter Lorimer", 
             "John Charles", 
             "Tom Jennings", 
-            "Allan Clarke", 
+            "Allan Clarke"
           ]
   },
   {     
         numb: 3,
-        questions: "Who was the latest Swedish player that played for Leeds United?",
+        question: "Who was the latest Swedish player that played for Leeds United?",
         answer: "Pontus Jansson",
-        alternative: [
+        options: [
             "Rasmus Kristensen",
             "Pontus Jansson", 
             "Tomas Brolin", 
-            "Brendan Aaronson", 
+            "Brendan Aaronson"
           ]
   },
   {
         numb: 4,
-        questions: "During the 19/20 season Leeds won the Championship but what was our total points?",
+        question: "During the 19/20 season Leeds won the Championship but what was our total points?",
         answer: "93",
-        alternative: [
+        options: [
             "93", 
             "92", 
             "91", 
-            "90", 
+            "90"
           ]
   },
   {
         numb: 5,
-        questions: "In the first season back in the Premier League with Bielsa, what rank did we end up at?",
+        question: "In the first season back in the Premier League with Bielsa, what rank did we end up at?",
         answer: "9",
-        alternative: [  
+        options: [  
             "12",
             "10",
             "9",
-            "11",
+            "11"
           ]
   },
   {
         numb: 6,
-        questions: "Who was the co-owner with Andrea Radrizzani before he bought the remaining 50%?",
+        question: "Who was the co-owner with Andrea Radrizzani before he bought the remaining 50%?",
         answer: "Massimo Cellino",
-        alternative: [  
+        options: [  
             "Mariano Chellini",
             "Matteo Claudio",
             "Maurizio Costa",
-            "Massimo Cellino",
+            "Massimo Cellino"
           ]
   },
   {
         numb: 7,
-        questions: "What answer is a nickname for Leeds United?",
+        question: "What answer is a nickname for Leeds United?",
         answers: "The Peacocks",
-        alternative: [
+        options: [
             "White Cockatoos",
             "The Doves",
             "The Peacocks",
-            "The Marchies",
+            "The Marchies"
           ]
   },
   {
         numb: 8,
-        questions: "What is the record attendance at Elland Road in 1967?",
+        question: "What is the record attendance at Elland Road in 1967?",
         answer: "57,892",
-        alternative: [  
+        options: [  
         "57,892",
         "55,625",
         "52,992",
-        "56,122",
+        "56,122"
           ]
   },
   {
         numb: 9,
-        questions: "How many members does Leeds United Supporters Club of Scandinavia have?",
+        question: "How many members does Leeds United Supporters Club of Scandinavia have?",
         answer: "5000+",
-        alternative: [
+        options: [
             "4000+",
             "2500+",
             "5000+",
-            "1500+",
+            "1500+"
           ]
   },
   {
         numb: 10,
-        questions: "Who is the most expensive player we have bought?",
+        question: "Who is the most expensive player we have bought?",
         answer: "Georgino Rutter",
-        alternative: [
+        options: [
             "Raphina",
             "Georgino Rutter",
             "Brendan Aaronson",
-            "Rodrigo Moreno",
+            "Rodrigo Moreno"
           ]
   },
   {
         numb: 11,
-        questions: "What team did we lose to in 2000/2001 Champions League semi-final?",
-        answer: "Valencia"
-        alternative: [
+        question: "What team did we lose to in 2000/2001 Champions League semi-final?",
+        answer: "Valencia",
+        options: [
             "Lazio",
             "Real Madrid",
             "Valencia",
-            "Deportivo La Coruña",
+            "Deportivo La Coruña"
           ]
   },
   {
         numb: 12,
-        questions: "What is the official fee we got when we sold Raphina to Barcelona (in pounds)?",
+        question: "What is the official fee we got when we sold Raphina to Barcelona (in pounds)?",
         answer: "£50,000,000",
-        alternative: [
+        options: [
             "£60,000,000",
             "£58,000,000",
             "£50,000,000",
-            "£55,000,000",
+            "£55,000,000"
           ]
   },
   {
         numb: 13,
-        questions: "How many Leeds players have been inducted into the English football Hall of fame?",
+        question: "How many Leeds players have been inducted into the English football Hall of fame?",
         answer: "11",
-        alternative: [
+        options: [
             "11",
             "8",
             "10",
-            "12",
+            "12"
           ]
   },
   {
         numb: 14,
-        questions: "During the 1960s what team inspired Revie to change our colours?",
+        question: "During the 1960s what team inspired Revie to change our colours?",
         answer: "Real Madrid",
-        alternative: [
+        options: [
             "Juventus",
             "Lyon",
             "Schalke 04",
-            "Real Madrid",
+            "Real Madrid"
           ]
   },
   {
         numb: 15,
-        questions: "What team did we beat in the FA Cup final 1972?",
+        question: "What team did we beat in the FA Cup final 1972?",
         answer: "Arsenal",
-        alternative:[
+        options: [
             "Tottenham",
             "Liverpool",
             "Arsenal",
-            "Derby",
+            "Derby"
           ]
   },
   {
         numb: 16,
-        questions: "How many goals did Patrick Bamford score during the 20/21 season back in the Premier League?",
+        question: "How many goals did Patrick Bamford score during the 20/21 season back in the Premier League?",
         answer: "17",
-        alternative: [
+        options: [
         "18",
         "15",
         "16",
-        "17",
+        "17"
           ]
   },
   {
         numb: 17,
-        questions: "What team did Marcelo Bielsa coach before Leeds United?",
+        question: "What team did Marcelo Bielsa coach before Leeds United?",
         answer: "Lille",
-        alternative: [
+        options: [
             "Lazio",
             "Lille",
             "Marseille",
-            "Athletic Bilbao",
+            "Athletic Bilbao"
           ]
   },
   {
         numb: 18,
-        questions: "What position had former Leeds coach David O'Leary as a player?",
+        question: "What position had former Leeds coach David O'Leary as a player?",
         answer: "Centre back",
-        alternative: [
+        options: [
             "Right back",
             "Midfielder",
             "Centre back",
-            "Left back",
+            "Left back"
           ]
   },
   {
         numb: 19,
-        questions: "In 2022 The Old Peacock pub changed its name temporarily to what?",
+        question: "In 2022 The Old Peacock pub changed its name temporarily to what?",
         answer: "The Bielsa",
-        alternative: [
+        options: [
             "The Dallas",
             "The Bamford",
             "Marching On",
-            "The Bielsa",
+            "The Bielsa"
           ]
   },
   {
         numb: 20,
-        questions: "Which shirt manufacturer did we have before Adidas took over 2020?",
+        question: "Which shirt manufacturer did we have before Adidas took over 2020?",
         answer: "Kappa",
-        alternative: [
+        options: [
             "Diadora",
             "Admiral",
             "Macron",
-            "Kappa",
+            "Kappa"
           ]
   }];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,47 @@
 // Get elements by id
 
-const welcomeRulesArea = document.getElementById("welcome-rules-area")
+const welcomeRulesArea = document.getElementById("welcome-rules-area");
 const questionArea = document.getElementById("quiz-area");
-const resultScoreArea = document.getElementById("score-area")
-const timer = document.getElementById("timer");
-const answerBtnArea = document.getElementById("answer-btn-area");
-const questionElement = document.getElementById("questions");
+const resultScoreArea = document.getElementById("score-area");
 const startQuizBtn = document.getElementById("start-quiz-btn");
 const nextQuestionBtn = document.getElementById("next-question-btn");
+const questionText = document.querySelector(".questions-class");
+const answerBtnArea = document.getElementById("answer-btn-area");
+const optionBtn = document.querySelector(".answer-button")
 
-let currentScore = 0;
-let currentQuestionIndex = 0;
+let questionCounter = 0;
+let currentQuestion = 0;
+let availableQuestions = [];
 
+
+
+
+/*
+function displayQuestion(index) {
+    questionText.innerHTML = quiz[1];
+    optionBtn.innerHTML = quiz[index].question;
+}
+*/
+
+function showQuestions(){
+    const que_text = document.querySelector(".questions-class");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ quiz[0].numb + ". " + quiz[0].question +'</span>';
+    let option_tag = 
+    '<div class="answer-button"><span>'+ quiz[0].options[0] +'</span></div>'
+    + '<div class="answer-button"><span>'+ quiz[0].options[1] +'</span></div>'
+    + '<div class="answer-button"><span>'+ quiz[0].options[2] +'</span></div>'
+    + optionBtn + quiz[0].options[3] +'</div>';
+    questionText.innerHTML = que_tag; //adding new span tag inside que_tag
+    optionBtn.innerHTML = option_tag; //adding new div tag inside option_tag
+    console.log(quiz[index].options[0])
+}
 
 // start button action
 startQuizBtn.onclick = ()=> {
-    welcomeRulesArea.style.display = 'none'; // hides the welcome and rules div
-    resultScoreArea.style.display = 'none'; // hides the result div
-    questionArea.style.display = 'block'; // shows the quiz with question and buttons
-    displayQuestions() // calling the function that shows the questions
-}
-
-// Next Question button action
-nextQuestionBtn.onclick = ()=> {
-    displayQuestions()
-}
-
-function displayQuestions(index) {
-    let questionTag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let answerTag = '<div class="option"><span>'+ questions[index].answer.text[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].answer.text[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].answer.text[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].answer.text[3] +'</span></div>';
-    questions.innerHTML = questionTag; //adding new span tag inside que_tag
-    answerBtnArea.innerHTML = answerTag; //adding new div tag inside option_tag
-
-    const answer = answerBtnArea.querySelectorAll(".answer");
+    welcomeRulesArea.style.display = 'none';
+    resultScoreArea.style.display = 'none';
+    questionArea.style.display = 'block';
+    showQuestions();
 }
