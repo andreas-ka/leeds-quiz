85835c92acd1009d969d0283a469107b73ee36b8
andreas-ka <>
2023-04-17 06:47:46 +0000

    troubleshooting answer button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const startQuizBtn = document.getElementById("start-quiz-btn");
 const nextQuestionBtn = document.getElementById("next-question-btn");
 const questionText = document.querySelector(".questions-class");
 const answerBtnArea = document.getElementById("answer-btn-area");
-const optionBtn = document.querySelector(".answer-button")
+const optionBtn = document.getElementsByClassName("answer-btn")
 
 let userScore = document.getElementById("correct-answers-counter")
 let questionCounter = 0;
@@ -16,7 +16,7 @@ let currentQuestion = 0;
 
 function showQuestions(){
     resetState()
-
+        
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>'+ quiz[0].numb + ". " + quiz[0].question +'</span>';
     let option_tag = 
@@ -26,6 +26,11 @@ function showQuestions(){
     + '<button class="answer-btn"><span>'+ quiz[0].options[3] +'</span></button>';
     questionText.innerHTML = que_tag; //adding new span tag inside question-class
     answerBtnArea.innerHTML = option_tag; // adding new span with option in answer buttons
+    optionBtn.onclick = alert("hello");
+
+     //validation code to see State field is mandatory.  
+    
+    
 }
 
 function resetState() {
